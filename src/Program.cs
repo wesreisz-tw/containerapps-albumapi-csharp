b0d14d6f3044a9e9b18815f0f43b2e28d48dd9da
Wesley Reisz <wes@wesleyreisz.com>
2024-09-08 20:39:09 -0400

    added new record

--- src/Program.cs
+++ src/Program.cs
@@ -47,7 +47,8 @@ record Album(int Id, string Title, string Artist, double Price, string Image_url
             new Album(3, "Scale It Up", "KEDA Club", 13.99, "https://aka.ms/albums-kedalogo"),
             new Album(4, "Lost in Translation", "MegaDNS", 12.99,"https://aka.ms/albums-envoylogo"),
             new Album(5, "Lock Down Your Love", "V is for VNET", 12.99, "https://aka.ms/albums-vnetlogo"),
-            new Album(6, "Sweet Container O' Mine", "Guns N Probeses", 14.99, "https://aka.ms/albums-containerappslogo")
+            new Album(6, "Sweet Container O' Mine", "Guns N Probeses", 14.99, "https://aka.ms/albums-containerappslogo"),
+            new Album(6, "Container-man' Mine", "Developers and Sea Shanties", 14.99, "http://www.wesleyreisz.com/images/logoWes.png"),
          };
 
         return albums; 
